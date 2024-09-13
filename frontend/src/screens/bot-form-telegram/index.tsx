19ea1f364de95f300ce53a89a0a9ba4e02218fda
Catalin Berta <catalin@mbp>
2024-09-13 17:19:52 +0300

    Remove unused element

--- frontend/src/screens/bot-form-telegram/index.tsx
+++ frontend/src/screens/bot-form-telegram/index.tsx
@@ -598,15 +598,6 @@ const TelegramBotForm: React.FC = () => {
 						</div>
 					</div>
 				</form>
-				{showFormSuccess && <div id="toast-success" className="flex self-center items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-					<div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
-						<svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-							<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-						</svg>
-						<span className="sr-only">Check icon</span>
-					</div>
-					<div className="ms-3 text-sm font-normal">Successfully updated.</div>
-				</div>}
 			</main>
 			<div className={`${showChunksModal ? '' : 'hidden'} relative z-10`} aria-labelledby="modal-title" role="dialog" aria-modal="true">
 				<div className="fixed inset-0 bg-gray-900 bg-opacity-80 transition-opacity" aria-hidden="true"></div>
